eeed4247a32a918f22a10062b9abfd1abb067bf9
ChihTeng Ma <chihtengma416@gmail.com>
2023-06-20 17:01:22 -0400

    Created Team.js
    Created Team model

---
+++ server/db/Team.js
@@ -0,0 +1,39 @@
+const conn = require("./conn");
+const { TEXT, UUIDV4, STRING, UUID } = conn.Sequelize;
+
+const Team = conn.define("team", {
+   id: {
+      type: UUID,
+      primaryKey: true,
+      defaultValue: UUIDV4
+   },
+   name: {
+      type: STRING,
+      allowNull: false,
+      validate: {
+         notEmpty: true
+      }
+   },
+   logo: {
+      type: STRING,
+      defaultValue: ""
+   },
+   description: {
+      type: TEXT
+   },
+   email: {
+      type: STRING,
+      allowNull: false,
+      validate: {
+         notEmpty: true,
+         isEmail: true
+      },
+      unique: true
+   },
+   leagueId: {
+      type: UUID,
+      allowNull: false
+   }
+});
+
+module.exports = Team;
