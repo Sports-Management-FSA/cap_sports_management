cce8c8f0807b7560606c16a50bcd031c5f28c92f
Mayra Sanchez <sanchezmayra901@gmail.com>
2023-06-20 14:55:26 -0700

    created Match model, turned db logging off

---
+++ server/db/models/Match.js
@@ -0,0 +1,71 @@
+const conn = require('../conn');
+const {TEXT, STRING, INTEGER, UUID, DATEONLY, TIME, BOOLEAN} = conn.Sequelize;
+
+const Match = conn.define('match', {
+    id: {
+        type: UUID,
+        primaryKey: true,
+    },
+    name: {
+        type: STRING,
+        allowNull: false,
+        unique: true,
+        validate:{
+            notEmpty: true,
+        }
+    },
+    description: {
+        type: TEXT,
+    },
+    date: {
+        type: DATEONLY,
+        allowNull: false,
+        validate:{
+            notEmpty: true,
+        }
+    },
+    time: {
+        type: TIME,
+        allowNull: false,
+        validate:{
+            notEmpty: true,
+        }
+    },
+    location:{
+        type: STRING,
+        allowNull: false,
+        validate:{
+            notEmpty: true,
+        }
+    },
+    teamAid:{
+        type: STRING,
+        allowNull: false,
+        validate:{
+            notEmpty: true,
+        }
+    },
+    teamBid:{
+        type: STRING,
+        allowNull: false,
+        validate:{
+            notEmpty: true,
+        }
+    },
+    teamAscore:{    
+        type: INTEGER,
+    },
+    teamBscore:{
+        type: INTEGER,
+    },
+    completion:{
+        type: BOOLEAN,
+        allowNull: false,
+        validate:{
+            notEmpty: true,
+        },
+        defaultValue: false,
+    },
+})
+
+module.exports = Match;
