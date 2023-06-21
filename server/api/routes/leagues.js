574fb9cd15aaadcd9b45d8ba47a66524b95a9be6
Allen <aharper1609@gmail.com>
2023-06-21 09:32:54 -0700

    leagues routes

--- server/api/routes/leagues.js
+++ server/api/routes/leagues.js
@@ -3,8 +3,44 @@ const { League } = require('../../db/models/League.js');
 
 router.get('/', async (req, res, next) => {
     try {
-        res.send('at /api/leagues')
+        const league = await League.findAll();
+        res.send(league);
     } catch(ex) {
+        next(ex);
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const league = await League.findByPk(req.params.id);
+        res.send(league);
+    } catch(ex) {
+        next(ex);
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        res.status(201).send(await League.create(req.body));
+    } catch(ex) {
+        next(ex);
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const league = await League.findByPk(req.params.id);
+        await league.update(req.body);
+    } catch(ex) {
+        next(ex)
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        await League.destroy({where: { id: req.params.id}});
+        res.sendStatus(204);
+    } catch(ex){
         next(ex);
     }
 })
