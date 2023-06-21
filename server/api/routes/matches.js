ef918d3c4ec6e7b3e1369c53de710f5002debe28
Mayra Sanchez <sanchezmayra901@gmail.com>
2023-06-21 12:16:14 -0700

    matches routes built out need authentication

--- server/api/routes/matches.js
+++ server/api/routes/matches.js
@@ -1,9 +1,48 @@
 const router = require('express').Router();
-const { Match } = require('../../db/models/Match.js');
+const { Match } = require('../../db/models/Match');
+const { User } = require('../../db/models/User');
 
 router.get('/', async (req, res, next) => {
     try {
-        res.send('at /api/matches')
+        const matches = await Match.findAll();
+        res.send(matches);
+    } catch(ex) {
+        next(ex);
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const match = await Match.findByPk(req.params.id);
+        res.send(match);
+    } catch(ex) {
+        next(ex);
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const user = await User.findByToken(req.headers.authorization);
+        
+    } catch(ex) {
+        next(ex);
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        await Product.destroy({where:{id: req.params.id}});
+        res.sendStatus(204);
+    } catch(ex) {
+        next(ex);
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        const match = await Match.findByPk(req.params.id);
+        await match.update(req.body);
+        res.send(match);
     } catch(ex) {
         next(ex);
     }
