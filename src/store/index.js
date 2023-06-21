2e5b42b313a94182f831fb315579e5437886822f
ChihTeng Ma <chihtengma416@gmail.com>
2023-06-21 17:12:38 -0400

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,25 @@
-import {configureStore} from "@reduxjs/toolkit";
-import logger from 'redux-logger';
-import auth from './auth';
-import teamsSlice from './teamsSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import auth from "./auth";
+import teamsSlice from "./teamsSlice";
 import matchesSlice from "./matchesSlice";
 import leaguesSlice from "./leaguesSlice";
-
+import playersSlice from "./playersSlice";
 
 const store = configureStore({
-  middleware: (defaultMiddleware)=> defaultMiddleware().concat(logger),
-  reducer:{
-    auth,
-    teams: teamsSlice,
-    matches: matchesSlice,
-    leagues: leaguesSlice
-  }
+   middleware: (defaultMiddleware) => defaultMiddleware().concat(logger),
+   reducer: {
+      auth,
+      teams: teamsSlice,
+      matches: matchesSlice,
+      leagues: leaguesSlice,
+      players: playersSlice
+   }
 });
 
 export default store;
-export * from './auth';
-export * from './teamsSlice';
-export * from './matchesSlice';
-export * from './leaguesSlice';
-
-
+export * from "./auth";
+export * from "./teamsSlice";
+export * from "./matchesSlice";
+export * from "./leaguesSlice";
+export * from "./playersSlice";
