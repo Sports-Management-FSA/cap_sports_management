c9db8a18c0939a1e5cd631722af2e3853da9355f
Allen <aharper1609@gmail.com>
2023-06-21 12:30:44 -0700

    added teamSlice to store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import logger from 'redux-logger';
 import auth from './auth';
+import teamSlice from './teamsSlice';
 
 const store = configureStore({
   middleware: (defaultMiddleware)=> defaultMiddleware().concat(logger),
   reducer:{
     auth,
+    teams: teamSlice
   }
 });
 
