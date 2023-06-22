08b9723c3156b6097c63721de4725c5bf2d6514a
ChihTeng Ma <chihtengma416@gmail.com>
2023-06-22 12:58:23 -0400

    Created Home Component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout } from '../store';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../store";
 
-const Home = ()=> {
-  const { auth } = useSelector(state => state);
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <h1>Home</h1>
+const Home = () => {
+   const { auth, leagues } = useSelector((state) => state);
+   const dispatch = useDispatch();
+
+   console.log(leagues.leaguesList);
+
+   return (
       <div>
-        Welcome { auth.username }!!
-        <button onClick={()=> dispatch(logout())}>Logout</button>
+         <h1>Home</h1>
+         <div>
+            Welcome {auth.username}!!
+            <button onClick={() => dispatch(logout())}>Logout</button>
+         </div>
+         <div>
+            {leagues.leaguesList[0].map((league, idx) => (
+               <div key={idx}>
+                  <h3>{league.name}</h3>
+                  <p>Season: {league.season}</p>
+               </div>
+            ))}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Home;
