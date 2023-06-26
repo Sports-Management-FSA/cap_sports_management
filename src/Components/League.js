bd091838281330205735f508ff792631493ecfeb
Allen <aharper1609@gmail.com>
2023-06-26 08:17:52 -0700

    fixed issue with Leage.js crashing on reload

--- src/Components/League.js
+++ src/Components/League.js
@@ -1,19 +1,31 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
+import { fetchAllLeagues } from "../store";
 
 const League = () => {
     const { id } = useParams();
+    const dispatch = useDispatch();
     const leagues = useSelector(state => state.leagues.leaguesList)
     const league = leagues.find(league => league.id == id);
     const teams = useSelector(state => state.teams.teamsList);
     const filteredTeams = teams.filter(team => team.leagueId == league.id);
-    const leagueId = league.id;
+    const leagueId = league ? league.id : null;
+
+    // useEffect(()=> {
+    //     dispatch(fetchAllLeagues())
+    // }, [dispatch, id])
+
+    if (!league){
+        return <div>...loading</div>
+    }
 
     return (
         <div className="league-container">
-            <h2>{league.logo}{league.name}</h2>
-            <h2>{league.season}</h2>
+            <div className="league__head">
+                <h2>{league.logo}{league.name}</h2>
+                <h2>{league.season}</h2>
+            </div>
             <Link to='/createteam' state={{ leagueId: leagueId}}>Create Team</Link>
             {
                 filteredTeams.map((team) => {
