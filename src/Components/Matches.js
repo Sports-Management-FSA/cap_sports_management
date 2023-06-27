1d42cb8cf1ffdf6b9fcfb1f63851671ed103bc4b
Sean <seanbennettdolan@gmail.com>
2023-06-26 20:47:39 -0400

    matches css looking pretty nice

--- src/Components/Matches.js
+++ src/Components/Matches.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { fetchAllMatches } from "../store";
+import { Link } from "react-router-dom";
 
 const Matches = () => {
     const matches = useSelector(state => state.matches.matchesList);
-    console.log(matches);
     const teams = useSelector(state => state.teams.teamsList);
-    console.log(teams);
-
+   
     var today = new Date(),
 
     date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
@@ -15,65 +14,100 @@ const Matches = () => {
     return(
         <div className='matches-container'>
             <div className='matches-header'>
-             <h1>Matches</h1>
+             <h2>Previous Matches</h2>
              </div>
-        <h5>Previous Matches</h5>
+        
         {
         matches.map((match) => {
             if (date > match.date) {
             return (
                 <div className='matches--match-container'>
                     <div className="matches--match" key={match.id}>
+                        <div className="matches--match-upper">
                     <div className="matches--match-upper-name">
-                        {match.name}
-                    
+                        <p>{match.name}</p>
+                        </div>
 <div className="matches--match-upper-dates">
 <p>{match.date} @ {match.time}</p>
        </div>
        </div>
+       
     <div className='matches--match-vs'>
         <div className="matches-match-vs-team">
-       <p>{teams.find(team => team.id === match.teamAid)?.name || ""} vs {teams.find(team => team.id === match.teamBid)?.name || ""}</p>
+       <p><Link to={`/teams/${match.teamAid}`}>{teams.find(team => team.id === match.teamAid)?.name || ""}</Link> vs <Link to={`/teams/${match.teamBid}`}>{teams.find(team => team.id === match.teamBid)?.name || ""}</Link></p>
+       </div>
        </div>
        <div className='matches--match-lower'>
        <p>{match.location}</p>
        <p>{match.description}</p>
        </div>
-        </div>
+        
         </div>
         </div>
 )}}
 )}
+<div className='matches-header'>
  <h2>Current Matches</h2>
+ </div>
  {
         matches.map((match) => {
             if (date === match.date) {
             return (
-                <section className='matches-features-center' key={match.id}>
-    <div className='matches-feature'>
-       <h4>{match.name}</h4>
-       <p>{match.date}</p>
-       <p>{match.time}</p>
-       <p>{match.location}</p>
-       <p>{match.description}</p>
-        </div>
-        </section>
+                <div className='matches--match-container'>
+                <div className="matches--match" key={match.id}>
+                    <div className="matches--match-upper">
+                <div className="matches--match-upper-name">
+                    <p>{match.name}</p>
+                    </div>
+<div className="matches--match-upper-dates">
+<p>{match.date} @ {match.time}</p>
+   </div>
+   </div>
+   
+<div className='matches--match-vs'>
+    <div className="matches-match-vs-team">
+   <p><Link to={`/teams/${match.teamAid}`}>{teams.find(team => team.id === match.teamAid)?.name || ""}</Link> vs <Link to={`/teams/${match.teamBid}`}>{teams.find(team => team.id === match.teamBid)?.name || ""}</Link></p>
+   </div>
+   </div>
+   <div className='matches--match-lower'>
+   <p>{match.location}</p>
+   <p>{match.description}</p>
+   </div>
+    
+    </div>
+    </div>
 )}}
 )}
+<div className='matches-header'>
         <h2>Upcoming Matches</h2>
+        </div>
         {
         matches.map((match) => {
             if (date < match.date) {
             return (
-                <section className='matches-features-center' key={match.id}>
-    <div className='matches-feature'>
-       <p>{match.name}</p>
-       <p>{match.date}</p>
-       <p>{match.time}</p>
-       <p>{match.location}</p>
-       <p>{match.description}</p>
-        </div>
-        </section>
+                <div className='matches--match-container'>
+                <div className="matches--match" key={match.id}>
+                    <div className="matches--match-upper">
+                <div className="matches--match-upper-name">
+                    <p>{match.name}</p>
+                    </div>
+<div className="matches--match-upper-dates">
+<p>{match.date} @ {match.time}</p>
+   </div>
+   </div>
+   
+<div className='matches--match-vs'>
+    <div className="matches-match-vs-team">
+   <p><Link to={`/teams/${match.teamAid}`}>{teams.find(team => team.id === match.teamAid)?.name || ""}</Link> vs <Link to={`/teams/${match.teamBid}`}>{teams.find(team => team.id === match.teamBid)?.name || ""}</Link></p>
+   </div>
+   </div>
+   <div className='matches--match-lower'>
+   <p>{match.location}</p>
+   <p>{match.description}</p>
+   </div>
+    
+    </div>
+    </div>
 )}}
 )}
     </div>
