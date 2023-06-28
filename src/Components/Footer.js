1e64bfdc158f709a23e5012a6c919ff2da199f51
ChihTeng Ma <chihtengma416@gmail.com>
2023-06-28 17:26:10 -0400

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,25 +2,25 @@ import React from "react";
 
 const Footer = () => {
    return (
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-         <p className="col-md-4 mb-0 text-white">© 2023 Podium</p>
-         <a
-            href="/"
-            className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-            <p className="bi me-2">Podium</p>
-         </a>
-         <ul className="nav col-md-4 justify-content-end">
-            <li className="nav-item">
-               <a href="#" className="nav-link px-2 text-muted">
-                  Home
-               </a>
-            </li>
-            <li className="nav-item">
-               <a href="#" className="nav-link px-2 text-muted">
+      <footer className="bg-dark">
+         <div
+            className="d-flex justify-content-between align-items-center mx-auto py-4 flex-wrap"
+            style={{ width: "80%" }}>
+            <a href="/#" className="d-flex align-items-center">
+               <i className="fa-solid fa-people-group fs-6" style={{ color: "#ffffff" }}>
+                  Podium
+               </i>
+            </a>
+            <small>&copy; Podium, {new Date().getFullYear()}. All rights reserved.</small>
+            <div>
+               <a href="/#" className="text-white">
                   About
                </a>
-            </li>
-         </ul>
+               <a href="/#" className="text-white ms-3">
+                  Team
+               </a>
+            </div>
+         </div>
       </footer>
    );
 };
