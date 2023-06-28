e83987df41b737fc75c695ae3036c0932e3d8aee
ChihTeng Ma <chihtengma416@gmail.com>
2023-06-28 17:06:54 -0400

    Created Footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+   return (
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+         <p className="col-md-4 mb-0 text-white">© 2023 Podium</p>
+         <a
+            href="/"
+            className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <p className="bi me-2">Podium</p>
+         </a>
+         <ul className="nav col-md-4 justify-content-end">
+            <li className="nav-item">
+               <a href="#" className="nav-link px-2 text-muted">
+                  Home
+               </a>
+            </li>
+            <li className="nav-item">
+               <a href="#" className="nav-link px-2 text-muted">
+                  About
+               </a>
+            </li>
+         </ul>
+      </footer>
+   );
+};
+
+export default Footer;
