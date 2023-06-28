fecabc1dd6d26122ce3a0238c51e8e0031393357
ChihTeng Ma <chihtengma416@gmail.com>
2023-06-28 12:53:08 -0400

    Updated Login

--- webpack.config.js
+++ webpack.config.js
@@ -14,17 +14,15 @@ module.exports = {
             test: /\.(png|jpe?g|gif)$/i,
             use: [
                {
-                  loader: 'file-loader',
+                  loader: "file-loader",
                   options: {
-                     name: '[name].[ext]',
-                     outputPath: 'images/',
-                     publicPath: 'images/',
-                  },
-               },
-            ],
-         },
-      ],
-   },
+                     name: "[name].[ext]",
+                     outputPath: "images/",
+                     publicPath: "images/"
+                  }
+               }
+            ]
+         }
+      ]
+   }
 };
-
-
