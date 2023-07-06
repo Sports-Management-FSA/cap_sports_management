5510d81e536b7936fd88fb0e5ea5f2b4f9358d14
ChihTeng Ma <chihtengma416@gmail.com>
2023-07-06 14:58:42 -0400

    updated Login theme to white

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -52,16 +52,22 @@ const Login = () => {
       <section
          className="vh-100"
          style={{
-            backgroundImage: "linear-gradient(to right, #434343 0%, black 100%)"
+            backgroundColor: "#e7ecef"
          }}>
          <div className="container py-5 h-100 ">
-            <form className="row d-flex justify-content-center align-items-center h-100 opacity-80">
+            <form className="row d-flex justify-content-center align-items-center h-100 opacity-90">
                <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                  <div className="card bg-dark text-white" style={{ borderRadius: "1rem" }}>
+                  <div
+                     className="card text-white"
+                     style={{
+                        borderRadius: "1rem",
+                        backgroundColor: "#fdfffc",
+                        border: "none"
+                     }}>
                      <div className="card-body p-5 text-center">
                         <div className="mb-md-5 mt-md-4 pb-5">
-                           <h2 className="fw-bold mb-2">Log into Podium</h2>
-                           <p className="text-white-50 mb-5">Please enter your username and password!</p>
+                           <h2 className="fw-bold mb-2 text-dark">Log into Podium</h2>
+                           <p className="text-dark mb-5">Please enter your username and password!</p>
                            <div className="form-outline form-white mb-4">
                               <input
                                  className="form-control form-control-lg"
@@ -82,34 +88,34 @@ const Login = () => {
                               />
                            </div>
                            <p className="small mb-5 pb-lg-2">
-                              <a className="text-white-50" href="#!">
+                              <a className="text-dark text-decoration-none forgot-password" href="#!">
                                  Forgot password?
                               </a>
                            </p>
                            {loginError && <p>{loginError}</p>}
                            <button
-                              className="btn btn-outline-light btn-lg px-5"
+                              className="btn btn-outline-dark btn-lg px-5"
                               disabled={invalidCredentials}
                               onClick={login}>
                               Login
                            </button>
                            <div className="d-flex justify-content-center text-center mt-4 pt-1">
-                              <a className="text-white" role="button" onClick={facebook}>
+                              <a className="text-dark" role="button" onClick={facebook}>
                                  <i className="fab fa-facebook-f fa-lg"></i>
                               </a>
-                              <a className="text-white" role="button" onClick={twitter}>
+                              <a className="text-dark" role="button" onClick={twitter}>
                                  <i className="fab fa-twitter fa-lg mx-4 px-2"></i>
                               </a>
 
-                              <a className="text-white" role="button" onClick={google}>
+                              <a className="text-dark" role="button" onClick={google}>
                                  <i className="fab fa-google fa-lg"></i>
                               </a>
                            </div>
                         </div>
                         <div>
-                           <p className="mb-0">
+                           <p className="mb-0 text-dark">
                               Don't have an account?{" "}
-                              <Link to="/register" className="text-decoration-none text-white-50 fw-bold">
+                              <Link to="/register" className="text-decoration-none text-dark fw-bold sign-up">
                                  Sign Up
                               </Link>
                            </p>
