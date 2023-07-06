94f5c8b27caf34f436ed562a1a89448adc3aa237
ChihTeng Ma <chihtengma416@gmail.com>
2023-07-06 12:32:09 -0400

    Fixed Bug

--- src/Components/Global/Footer.js
+++ src/Components/Global/Footer.js
@@ -6,17 +6,17 @@ const Footer = () => {
          <div
             className="d-flex justify-content-between align-items-center mx-auto py-4 flex-wrap"
             style={{ width: "80%" }}>
-            <a href="/#" className="d-flex align-items-center">
-               <i className="fa-solid fa-people-group fs-6" style={{ color: "#ffffff" }}>
+            <a href="/#" className="d-flex align-items-center text-decoration-none">
+               <i className="fa-solid fa-people-group fs-6 text-decoration-none" style={{ color: "#ffffff" }}>
                   Podium
                </i>
             </a>
             <small className="text-white">&copy; Podium, {new Date().getFullYear()}. All rights reserved.</small>
             <div>
-               <a href="/#" className="text-white">
+               <a href="/#" className="text-white text-decoration-none">
                   About
                </a>
-               <a href="/#" className="text-white ms-3">
+               <a href="/#" className="text-white ms-3 text-decoration-none">
                   Team
                </a>
             </div>
