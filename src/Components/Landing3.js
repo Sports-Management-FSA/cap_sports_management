a6b42360835993e24f0bf9f7d81c30aa27c239c9
Sean <seanbennettdolan@gmail.com>
2023-07-12 18:04:41 -0400

    updates to the landing page

--- src/Components/Landing3.js
+++ src/Components/Landing3.js
@@ -8,7 +8,7 @@ const Landing3 = () => {
 <div className="body">
     <section id="section1">
       <div className="landing_container">
-        <article>
+        <div className="landing_text_photo">
          <div className="landing_header">
             <h1>Build.</h1>
             <h1>Play.</h1>
@@ -17,25 +17,42 @@ const Landing3 = () => {
             </div>
 
              <div className="landing_photo"></div> 
-            </article>
-            <p className="section1-description">
+            </div>
+
+            <div className="section1-description">
                {/* Unlock the realm of competitive sports and esports.  */}
-               
+               <h5>
                 Effortlessly create and manage leagues and teams, or
                seamlessly join existing ones. Engage in real-time communication, chat with fellow players, and stay
                connected to your community. Dive into comprehensive statistics and receive live updates on the latest
                game stats, keeping you at the forefront of every thrilling moment.
-            </p>
+               </h5>
+            </div>
             </div>
             </section>
             <section id="section2">
                 <div className="section2_photos">
-                <div className="home_photo"></div>
-                <div className="leagues_photo"></div>
-                <div className="scorekeeper_photo"></div>
+                <div className="home_photo">
+<div className="cover">
+<Link to="/home">Join a league and start a team now</Link>
+</div>
+
                 </div>
-                <div className="links_container">
-                <p>
+                <div className="leagues_photo">
+<div className="cover">
+<Link to="/matches">Checkout some upcoming matches</Link>
+</div>
+
+                </div>
+                <div className="scorekeeper_photo">
+<div className="cover">
+<Link to="/scorekeeper">Keep track of the game with our scorekeeper feature</Link>
+</div>
+
+                </div>
+                </div>
+                {/* <div className="links_container"> */}
+                {/* <p>
                   <Link to="/home">Join a league and start a team now</Link>
                </p>
                <p>
@@ -43,8 +60,8 @@ const Landing3 = () => {
                </p>
                <p>
                   <Link to="/scorekeeper">Keep track of the game with our scorekeeper feature</Link>
-               </p>
-               </div>
+               </p> */}
+               {/* </div> */}
             </section>
             </div>
         
