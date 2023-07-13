268846a1159c634ae66c997c0d92c2f8b9b94b3a
ChihTeng Ma <chihtengma416@gmail.com>
2023-07-12 21:47:01 -0400

    Updated User Profile
    Added account details functionality

--- src/Components/User/UserProfileAccountDetail.js
+++ src/Components/User/UserProfileAccountDetail.js
@@ -1,7 +1,48 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { loginWithToken } from "../../store";
 
 const UserProfileAccountDetail = () => {
+   const { auth } = useSelector(({ auth }) => ({ auth }));
+   const fileInputRef = useRef(null);
+   const dispatch = useDispatch();
+   const [formData, setFormData] = useState({
+      username: auth.username || "",
+      firstName: auth.firstName || "",
+      lastName: auth.lastName || "",
+      email: auth.email || "",
+      avatar: auth.avatar || ""
+   });
+
+   // Account Details
+   const handleInputChange = (ev) => {
+      setFormData({ ...formData, [ev.target.id]: ev.target.value });
+   };
+
+   // User Avatar
+   const [previewAvatar, setPreviewAvatar] = useState("");
+
+   const handleUploadButtonClick = () => fileInputRef.current.click();
+   const handleFileInputChange = (ev) => {
+      if (ev.target && ev.target.files.length > 0) {
+         const file = ev.target.files[0];
+         const reader = new FileReader();
+         reader.readAsDataURL(file);
+         reader.addEventListener("load", () => {
+            setFormData({ ...formData, avatar: reader.result });
+            setPreviewAvatar(reader.result);
+         });
+      }
+   };
+
+   useEffect(() => {
+      dispatch(loginWithToken());
+   }, [dispatch]);
+
+   const handleSubmit = (ev) => {
+      ev.preventDefault();
+   };
+
    return (
       <div className="row">
          <div className="col-xl-4">
@@ -9,12 +50,21 @@ const UserProfileAccountDetail = () => {
                <div className="card-header">Profile Picture</div>
                <div className="card-body text-center">
                   <img
-                     src="static/images/camera.svg"
+                     src={formData.avatar ? formData.avatar : "static/images/camera.svg"}
                      alt="profile-picture"
                      className="img-account-profile rounded-circle mb-2"
                   />
                   <div className="small font-italic text-muted mb-4">JPG or PNG no larger than 5 MB</div>
-                  <button className="btn btn-outline-secondary">Upload new image</button>
+                  <button className="btn btn-outline-secondary" onClick={handleUploadButtonClick}>
+                     Upload new image
+                  </button>
+                  <input
+                     type="file"
+                     id="uploadInput"
+                     className="d-none"
+                     ref={fileInputRef}
+                     onChange={handleFileInputChange}
+                  />
                </div>
             </div>
          </div>
@@ -22,7 +72,7 @@ const UserProfileAccountDetail = () => {
             <div className="card mb-4">
                <div className="card-header">Account Details</div>
                <div className="card-body">
-                  <form>
+                  <form onSubmit={handleSubmit}>
                      <div className="row">
                         <div className="mb-1">
                            <label className="small mb-1" htmlFor="username">
@@ -33,6 +83,8 @@ const UserProfileAccountDetail = () => {
                               type="text"
                               id="username"
                               placeholder="Username can not be empty"
+                              value={formData.username}
+                              onChange={handleInputChange}
                            />
                         </div>
                      </div>
@@ -46,6 +98,8 @@ const UserProfileAccountDetail = () => {
                               type="text"
                               id="firstName"
                               placeholder="FirstName can not be empty"
+                              value={formData.firstName}
+                              onChange={handleInputChange}
                            />
                         </div>
                         <div className="col-md-6">
@@ -57,6 +111,8 @@ const UserProfileAccountDetail = () => {
                               type="text"
                               id="lastName"
                               placeholder="LastName can not be empty"
+                              value={formData.lastName}
+                              onChange={handleInputChange}
                            />
                         </div>
                      </div>
@@ -70,6 +126,8 @@ const UserProfileAccountDetail = () => {
                               type="email"
                               id="email"
                               placeholder="Email can not be empty"
+                              value={formData.email}
+                              onChange={handleInputChange}
                            />
                         </div>
                      </div>
