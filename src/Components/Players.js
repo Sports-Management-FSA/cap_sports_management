99d8dfafee1178e57dcfceb83e36a0c49201d13c
Mayra Sanchez <sanchezmayra901@gmail.com>
2023-07-13 23:24:24 -0700

    added messages to league stats subtabs if no info to display

--- src/Components/Players.js
+++ src/Components/Players.js
@@ -5,25 +5,31 @@ const Players = (props) => {
     const {players, teams} = props;
 
     return (
-        <table className="table">
-            <thead>
-                <tr className="table__header-container">
-                    <th className="table__header">Team</th>
-                    <th className="table__header">Name</th>
-                </tr>
-            </thead>
-            <tbody className="table__body-container">
-           {
-            players.map(member=>{
-                return(
-                <tr key={member.id}>
-                    <td className="table__cell">{teams.find(team=>team.id==member.teamId).name}</td>
-                    <td className="table__cell">{member.user.firstName} {member.user.lastName}</td>
-                </tr>
-                    )})
-           }
-           </tbody>
-        </table>
+        <div>
+            {players.length > 0 ?
+            <table className="table">
+                <thead>
+                    <tr className="table__header-container">
+                        <th className="table__header">Team</th>
+                        <th className="table__header">Name</th>
+                    </tr>
+                </thead>
+                <tbody className="table__body-container">
+            {
+                players.map(member=>{
+                    return(
+                    <tr key={member.id}>
+                        <td className="table__cell">{teams.find(team=>team.id==member.teamId).name}</td>
+                        <td className="table__cell">{member.user.firstName} {member.user.lastName}</td>
+                    </tr>
+                        )})
+            }
+            </tbody>
+            </table>
+            :
+            <p>No Players to Display</p>
+            }
+        </div>
     );
 };
 
--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -4,29 +4,35 @@ const Teams = (props) => {
     const {teams} = props;
 
     return (
-        <table className="table">
-            <thead>
-                <tr className="table__header-container">
-                    <th className="table__header"></th>
-                    <th className="table__header">Team Name</th>
-                    <th className="table__header">Total Players</th>
-                </tr>
-            </thead>
-            <tbody className="table__body-container">
-           {
-            teams.map(team=>{
-                const numPlayers = team.user_teamRoles.filter(player=>(player.teamRole.name == 'player')).length;
-                return(
-                        <tr key={team.id}>
-                            <td className="table__cell"><img src={window.location.origin + `${team.logo}`} width="20" height="10" alt="Image" /> </td>
-                            <td className="table__cell">{team.name}</td>
-                            <td className="table__cell">{numPlayers}</td>
-                        </tr>
-                )
-            })
-           }
-           </tbody>
-        </table>
+        <div>
+            {teams.length > 0 ?
+            <table className="table">
+                <thead>
+                    <tr className="table__header-container">
+                        <th className="table__header"></th>
+                        <th className="table__header">Team Name</th>
+                        <th className="table__header">Total Players</th>
+                    </tr>
+                </thead>
+                <tbody className="table__body-container">
+            {
+                teams.map(team=>{
+                    const numPlayers = team.user_teamRoles.filter(player=>(player.teamRole.name == 'player')).length;
+                    return(
+                            <tr key={team.id}>
+                                <td className="table__cell"><img src={window.location.origin + `${team.logo}`} width="20" height="10" alt="Image" /> </td>
+                                <td className="table__cell">{team.name}</td>
+                                <td className="table__cell">{numPlayers}</td>
+                            </tr>
+                    )
+                })
+            }
+            </tbody>
+            </table> 
+            :
+            <p>No Teams to Display</p>
+            }
+        </div>
     );
 };
 
