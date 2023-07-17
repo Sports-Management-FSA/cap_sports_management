959f6c6682f213a2a5e0cadca77ea81ac21533e3
Mayra Sanchez <sanchezmayra901@gmail.com>
2023-07-16 22:40:23 -0700

    fixed player getting added to team

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -58,20 +58,28 @@ router.delete("/:id", async (req, res, next) => {
 });
 
 router.put("/:id", async (req, res, next) => {
-  console.log("FROM API", req.body)
   try {
     const user = await User.findByPk(req.params.id);
     if(req.body.role){
       const {role} = req.body;
-      if(role.teamRoleId){
-        User_TeamRoles.create({userId: user.id, teamId: role.teamId, teamRoleId: role.roleId});
+      if(role.roleId){
+        await User_TeamRoles.create({userId: user.id, teamId: role.teamId, teamRoleId: role.roleId});
       } else {
-        User_LeagueRoles.create({userId: user.id, teamId: role.teamId, teamRoleId: role.roleId})
+        await User_LeagueRoles.create({userId: user.id, teamId: role.teamId, leagueRoleId: role.roleId})
       }
     } else {
       user.update(req.body);
     }
-    res.send(user);
+    const updatedUser = await User.findByPk(req.params.id, {
+      include: [ 
+        {model: Requests, as: 'receivedRequests'},
+        {model: User_LeagueRoles, include: [League, LeagueRoles]},
+        {model: User_TeamRoles, include: [Team, TeamRoles]},
+        {model: Scorekeeper, include: [Actions, Team, Match]},
+        { model: Post, include: [Comment] },
+      ],
+    });
+    res.send(updatedUser);
   } catch (err) {
     next(err);
   }
